aed77cbe94ab41b9d97d227df13284f7d7e6e7b4
Lucas Rodrigues <lucasbrsilva@gmail.com>
2018-05-21 21:34:02 -0300

    Início da separação do serviço BTG360

---
+++ AlliNMobileSwift/AlliNMobileSwift/BTG360.swift
@@ -0,0 +1,71 @@
+//
+//  BTG360.swift
+//  AlliNMobileSwift
+//
+//  Created by Lucas Rodrigues on 21/05/2018.
+//  Copyright © 2018 Lucas Rodrigues. All rights reserved.
+//
+
+import Foundation
+
+public class BTG360 {
+    public static func initialize(deviceToken: String) {
+    
+    }
+    
+    public static func addProduct(account: String) {
+        
+    }
+    
+    public static func addProducts(account: String) {
+        
+    }
+    
+    public static func addCart(account: String) {
+        
+    }
+    
+    public static func addCarts(account: String) {
+        
+    }
+    
+    public static func addClient(account: String) {
+        
+    }
+    
+    public static func addClients(account: String) {
+        
+    }
+    
+    public static func addTransaction(account: String) {
+        
+    }
+    
+    public static func addTransactions(account: String) {
+        
+    }
+    
+    public static func addSearch(account: String) {
+        
+    }
+    
+    public static func addSearchs(account: String) {
+        
+    }
+    
+    public static func addWishList(account: String) {
+        
+    }
+    
+    public static func addWishLists(account: String) {
+        
+    }
+    
+    public static func addWarnMe(account: String) {
+        
+    }
+    
+    public static func addWarneMes(account: String) {
+        
+    }
+}
