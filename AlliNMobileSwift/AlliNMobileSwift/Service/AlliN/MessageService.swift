8a307bfe4b87f81c9f4a4f98a499b9ea725e30b8
Lucas Rodrigues <lrodrigues@allin.com.br>
2019-02-28 16:14:54 -0300

    Remoção de classes não utilizadas

--- AlliNMobileSwift/AlliNMobileSwift/Service/AlliN/MessageService.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MessageService.swift
-//  AlliNMobileSwift
-//
-//  Created by Lucas Rodrigues on 12/06/17.
-//  Copyright © 2017 Lucas Rodrigues. All rights reserved.
-//
-class MessageService {
-}
