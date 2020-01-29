61be3780bccfef86f5e891b07741af119c04583d
Lucas Rodrigues <lrodrigues@allin.com.br>
2020-01-29 09:15:56 -0300

    Remoção de envio automático

--- AlliNMobileSwift/AlliNMobileSwift/Constant/DefaultListConstant.swift
+++ AlliNMobileSwift/AlliNMobileSwift/Constant/DefaultListConstant.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2017 Lucas Rodrigues. All rights reserved.
 //
 struct DefaultListConstant {
-    static let LISTA_PADRAO = "Lista Padrao Push";
     static let PUSH_ID = "push_id";
     static let PLATAFORMA = "plataforma";
 }
